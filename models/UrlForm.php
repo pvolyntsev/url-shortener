bb7b85b9fbadf0496250a6021981533cb4465ea7
Pavel Volyntsev <pvolyntsev@users.noreply.github.com>
2013-11-12 15:05:27 +0700

    Update UrlForm.php

--- models/UrlForm.php
+++ models/UrlForm.php
@@ -1,9 +1,21 @@
 <?php
 
+/**
+ * Web form with to input elements: 'url' and 'shortUrl'
+ * 
+ * It will render using view /views/forms/url.phtml
+ */
 class UrlForm extends AppForm {
 
 	protected $_method = 'POST';
 
+	/**
+	 * Returns list of form elements with extra info: text label, data type, list of filters and list of validators
+	 * 
+	 * Filters and validators will be applied when call $this->setValue('elementName', 'newValue');
+	 * Filters will remove wrong characters or convert element value
+	 * Validators will check element value to have correct content
+	 */
 	public function getRules() {
 		return array(
 			'url' => array(
